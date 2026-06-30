b06093dfe32ad0c39c83c170ac33db7a81bf01fe
VanZdev <139448115+ZlaIvaDev@users.noreply.github.com>
2026-06-30 13:46:19 +0900

    updated item page

--- listitemjs.js
+++ listitemjs.js
@@ -1,2 +1,19 @@
 console.log('items checkuo!');
 
+const modalbtn = document.getElementById('openmodal');
+modalbtn.addEventListener('click', function event() {
+    console.log('clicked modal button')
+});
+
+/* napisati funkciju show modal koja izbacuje modal meni */
+/* funkcija ce imati polja za unos, */
+/* i meni sa izborom prioriteta (high/medium/low/ completed) */
+
+/* takodje, napisati create modal funkc ?
+ili jednostavno dodati modal u html */
+
+
+
+/* js za padajuci meni */
+
+/* prouci ga kasnije ! tj ujutru hvala !*/
